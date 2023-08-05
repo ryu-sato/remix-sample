04d79961006416d9ce4686de1bb942b2dabd7597
Ryu Sato <ryu@weseek.co.jp>
2023-08-05 08:31:28 +0000

    react-dnd,react-dnd-html5-backend削除

--- app/domains/tasks/DraggableTask.tsx
+++ app/domains/tasks/DraggableTask.tsx
@@ -1,25 +1,12 @@
 import type { Task } from "@prisma/client";
 import type { SerializeFrom } from "@remix-run/node";
-// import { useDrag } from "react-dnd";
 
 export default function DraggableTask(task: SerializeFrom<Task>) {
-  // const [{ opacity }, dragRef] = useDrag(
-  //   () => ({
-  //     type: 'TASK',
-  //     item: { id: task.id },
-  //     collect: (monitor) => ({
-  //       opacity: monitor.isDragging() ? 0.5 : 1
-  //     })
-  //   }),
-  //   []
-  // )
-
   if (task == null) {
     return <></>
   }
 
   return (
-    // <div key={ task.id } ref={ dragRef } style={{ opacity }}>
     <div>
       { task.title }
     </div>
