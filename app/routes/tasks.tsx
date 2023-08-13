875e3ed49c2fec94d0d32812ea7e33aca3aa0318
Ryu Sato <ryu@weseek.co.jp>
2023-08-13 09:42:57 +0000

    使っていない引数を削除

--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -2,7 +2,7 @@ import type { ActionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { db } from "~/services/db.server";
 
-export const action = async ({ request, params }: ActionArgs) => {
+export const action = async ({ request }: ActionArgs) => {
   const { title, body, swimlaneId } = await request.json() as unknown as { title: string, body: string, swimlaneId: number };
   if (title == null || body == null || swimlaneId == null) {
     return json({ status: 400 });
